3f394379426cd471cc7456eea260f4a1b1b7c437
GameplaySurYtb <36791801+GameplaySurYtb@users.noreply.github.com>
2018-02-24 14:55:07 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,13 +1,20 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
+bot.on("ready", function(){
+    bot.user.setGame("ElyBot, //help")
+    message.message("ElyBot activé")
+})
+bot.on('guildMemberAdd', function (member){
+    member.createDM("Coucou").then(function (channel){
+        return channel.send('Bienvenue sur le channel' + member.displayName)
+    })
+})
+
 bot.on("message", function (message){
-    if_(message.content === "//help")
+    if(message.content === "//help")
        message.reply("Ya pas de commandes")
 })
-bot.on("ready", function(){
-    bot.user.setGame("ElyBot, //help");
-    message.reply("ElyBot activé");
-});
+
 
 bot.login("NDA3MTI1MzYxNTk2NDMyMzg0.DXL5CQ.jMf7MepV70DwT-DQHQFinzlQdh8");
