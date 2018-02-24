7c9319eac36e98798f96283384a776ec7b7cdc69
GameplaySurYtb <36791801+GameplaySurYtb@users.noreply.github.com>
2018-02-24 14:01:12 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function(){
+    bot.user.setGame("ElyBot, !help");
+    console.log("Le bot a bien ete conecté");
+});
+
+bot.login("NDA3MTI1MzYxNTk2NDMyMzg0.DXLmSw.VkWXXvrlpbquQRxpdTsOHyovGkU");
