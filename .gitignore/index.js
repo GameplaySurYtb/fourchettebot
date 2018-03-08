f51599de9ac48c69b795cbc9ef2864b6dadc635c
GameplaySurYtb <36791801+GameplaySurYtb@users.noreply.github.com>
2018-03-08 17:05:53 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -33,26 +33,7 @@ bot.on("message", message => {
         bot.user.setGame(command.substr(9));
     } else if(command === "!!resetgame"){
         bot.user.setGame("ElyBot, !!help")
-        message.channel.sendMessage(message.member + " Tu viens de reset le Jeux du bot")
-    } else if(command === "!!kick"){
-        let modRole = message.guild.roles.find("name", "[Fondateur]")
-        if(!message.member.roles.has(modRole.id)){
-            return message.reply("Tu t'es pris pour un admin ?").catch(console.error)
-        }
-        if(message.mentions.user.size === 0){
-            return message.reply("Merci de mentionner le joueur a kick").catch(console.error)
-        }
-        let kickMember = message.guild.member(message.mentions.user.first());
-        if(!kickMember){
-            return message.reply("Joueur Introuvable ou impossible a expulser");
-        }
-        if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")){
-            return message.reply("Je ne peus pas kick de joueurs ccar je n'ai pas la perm").catch(console.error)
-        }
-        kickMember.kick().then(member =>{
-            message.reply(`${member.user.username} a été kick avec succes`).catch(console.error);
-            message.guild.channels.find("name", "accueil").send(`**${member.user.username} a été expulsé du discord par **${message.author.username}`)
-        }).catch(console.error)
+        message.channel.sendMessage(message.member + " Tu viens de reset le Jeux du bot");
     }
 });
 bot.on("guildMemberAdd", member => {
