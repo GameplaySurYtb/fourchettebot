145d5dcb27167a86685154385c56911bd5676564
GameplaySurYtb <36791801+GameplaySurYtb@users.noreply.github.com>
2018-03-10 19:20:01 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,40 +3,44 @@ var bot = new Discord.Client();
 
 bot.on("ready", function() {
     bot.user.setGame("ElyBot !!help");
-    bot.user.setUsername("ElyBot");
 });
 bot.on("message", message => {
     if(message.content === "!!help"){
         message.channel.sendMessage(message.member + " Les commandes t'ont étés envoyés en MP");
         message.author.createDM().then(channel => {
-            channel.send("Commandes disponnibles sur ELYTHIOM :\n \n```Markdown\n!!help -> Avoir la liste des commandes\n```")
+            channel.send("Commandes disponnibles sur ELYTHIOM :\n \n```Markdown\n!!help -> Avoir la liste des commandes\n```");
         });
         console.log("Commande !!help effectuée par :\n" + message.author.displayName);
-    } else if(command === "coucou"){
+    } else if(message.content === "coucou"){
         message.channel.sendMessage("coucou " + message.member);
         message.author.createDM().then(channel => {
             channel.send("╔════════════════════════════╗\n║                                   COUCOU                                      ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n╚════════════════════════════╝")
         });
-    } else if(command === "COUCOU"){
+    } else if(message.content === "COUCOU"){
         message.channel.sendMessage("coucou " + message.member);
         message.author.createDM().then(channel => {
             channel.send("╔════════════════════════════╗\n║                                   COUCOU                                      ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n╚════════════════════════════╝")
         });
-    } else if(command === "Coucou"){
+    } else if(message.content === "Coucou"){
         message.channel.sendMessage("coucou " + message.member);
         message.author.createDM().then(channel => {
             channel.send("╔════════════════════════════╗\n║                                   COUCOU                                      ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n╚════════════════════════════╝")
         });
-    } else if(command.startsWith("!!setgame")){
-        bot.user.setGame(command.substr(9));
-    } else if(command === "!!resetgame"){
+    } else if(message.content.startsWith("!!setgame")){
+        bot.user.setGame(message.content.substr(9));
+    } else if(message.content === "!!resetgame"){
         bot.user.setGame("ElyBot, !!help")
         message.channel.sendMessage(message.member + " Tu viens de reset le Jeux du bot")
+    } else if(message.content === "!tamere"){
+        message.channel.sendMessage("Va niquer ta mere ptit enculé")
+    } else if(message.content === "fdp ta mere la pute"){
+        message.reply("Je vais le dire a ma maman")
+        message.react(Emoji)
     }
 });
 bot.on("guildMemberAdd", member => {
     member.createDM().then(channel => {
         return channel.send("Bienvenue sur ELYTHIOM " + member.displayName + "\n \nSi tu veus la liste des commandes; va sur le discord de ElyThiom et fais !!help")
     })
-})
+});
 bot.login(process.env.TOKEN);
