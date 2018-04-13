94ba36e414c989defc5332ad82f669fe9f5d855f
GameplaySurYtb <36791801+GameplaySurYtb@users.noreply.github.com>
2018-04-13 23:28:28 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("ElyBot, !!help");
+    bot.user.setGame("Fortnite (!help)");
 });
 bot.on("message", message => {
-    if(message.content === "!!help"){
+    if(message.content === "!help"){
         message.channel.sendMessage(message.member + " Les commandes t'ont étés envoyés en MP");
         message.author.createDM().then(channel => {
             channel.send("Commandes disponnibles sur ELYTHIOM :\n \n```Markdown\n!!help -> Avoir la liste des commandes\n```");
@@ -26,10 +26,10 @@ bot.on("message", message => {
         message.author.createDM().then(channel => {
             channel.send("╔════════════════════════════╗\n║                                   COUCOU                                      ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n║                                                                                           ║\n╚════════════════════════════╝")
         });
-    } else if(message.content.startsWith("!!setgame")){
+    } else if(message.content.startsWith("!setgame")){
         bot.user.setGame(message.content.substr(9));
-    } else if(message.content === "!!resetgame"){
-        bot.user.setGame("ElyBot, !!help")
+    } else if(message.content === "!resetgame"){
+        bot.user.setGame("fortnite (!help)")
         message.channel.sendMessage(message.member + " Tu viens de reset le Jeux du bot")
     } else if(message.content === "!tamere"){
         message.channel.sendMessage("Va niquer ta mere ptit enculé")
@@ -40,7 +40,7 @@ bot.on("message", message => {
 });
 bot.on("guildMemberAdd", member => {
     member.createDM().then(channel => {
-        return channel.send("Bienvenue sur ELYTHIOM " + member.displayName + "\n \nSi tu veus la liste des commandes; va sur le discord de ElyThiom et fais !!help")
+        return channel.send("Bienvenue sur LE DSICORD DE GARRIS " + member.displayName + "\n \nSi tu veus la liste des commandes; va sur le discord de Garris et fais !help")
     })
 });
 bot.login(process.env.TOKEN);
